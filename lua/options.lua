e81267a5b2444b06ee6d2903b63cb23afd85641e
James Huang <james.huang@proptrack.com>
2025-04-25 21:18:43 +1000

    feat: set foldmethod and add vim fugitive

--- lua/options.lua
+++ lua/options.lua
@@ -12,6 +12,9 @@ vim.opt.number = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
+-- Enable fold method - more indent means a higher fold level
+vim.opt.foldmethod = 'indent'
+
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
