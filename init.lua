2bd9f264b07815813bf50134de377f52693c8fee
James Huang <james.huang@proptrack.com>
2025-11-24 19:58:46 +1100

    feat: update to github theme

--- init.lua
+++ init.lua
@@ -179,9 +179,16 @@ require('lazy').setup({
 
   -- Color theme
   {
-    'EdenEast/nightfox.nvim',
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      vim.cmd 'colorscheme nightfox'
+      require('github-theme').setup {
+        -- ...
+      }
+
+      vim.cmd 'colorscheme github_dark_dimmed'
     end,
   },
 
